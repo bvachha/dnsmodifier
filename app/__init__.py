bd454de4a0c1c4a219d09d1a697db0153a3a9902
Bakhtyar <bakhtyar.vachha@in.ey.com>
2021-02-14 19:16:38 +0530

    application initialization code. Added new config parameters for logger module added mappings to the SQLAlchemy orm library

---
+++ app/__init__.py
@@ -0,0 +1,36 @@
+from logging.config import dictConfig
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
+
+# Configuration for the application logs to stream handler and log file handler
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }},
+    'handlers': {
+        'wsgi': {
+            'class': 'logging.StreamHandler',
+            'stream': 'ext://flask.logging.wsgi_errors_stream',
+            'formatter': 'default'
+        },
+        'wsgi_file': {
+            'class': 'logging.FileHandler',
+            'filename': 'application.logs'
+        }
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi', 'wsgi_file']
+    }
+})
+
+# Initialization of the flask plugins
+app = Flask(__name__)
+app.config.from_object(Config)  # load config into flask app
+db = SQLAlchemy(app)
+
+from app import routes
