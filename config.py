95b11c9f61dfc7b20a36d5035b6a1b75352171fe
Bakhtyar <bakhtyar.vachha@in.ey.com>
2021-02-22 09:16:48 +0530

    removed unused env variables for the dns api service

--- config.py
+++ config.py
@@ -8,17 +8,10 @@ class Config(object):
     SECRET_KEY = os.environ.get('APP_SECRET_KEY')
     if not SECRET_KEY:
         raise ValueError("Please set the Application secret key value in the environment")
-    DNS_WEB_ADDRESS = os.environ.get('DNS_WEB_ADDRESS')
-    if not DNS_WEB_ADDRESS:
-        raise ValueError("Please set the DNS API server Address in the environment")
-    DNS_API_KEY = os.environ.get('DNS_API_KEY')
-    if not DNS_API_KEY:
-        raise ValueError("Please set the DNS API key in the environment")
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if not SQLALCHEMY_DATABASE_URI:
         raise ValueError("Please set the DNS db path in the environment")
     API_KEY = os.environ.get('API_KEY')
     if not API_KEY:
         raise ValueError("Please set the API_KEY in the environment")
-    DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
